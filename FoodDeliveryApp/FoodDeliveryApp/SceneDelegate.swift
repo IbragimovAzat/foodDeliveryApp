a4d709ed061226eb55ed9f4e8015f80b2fb980da
IbragimovAzat <appleses@icloud.com>
2024-05-24 21:39:21 +0500

    Added new logo for app

--- FoodDeliveryApp/FoodDeliveryApp/SceneDelegate.swift
+++ FoodDeliveryApp/FoodDeliveryApp/SceneDelegate.swift
@@ -19,7 +19,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         let viewController = ViewController()
-        viewController.view.backgroundColor = .green
         let navigationController = UINavigationController(rootViewController: viewController)
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
