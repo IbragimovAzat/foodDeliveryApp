341a5175bd4eb14b93e6458da00596f12186f72f
IbragimovAzat <appleses@icloud.com>
2024-06-03 15:48:05 +0500

    Added screens for onboarding

---
+++ FoodDeliveryApp/FoodDeliveryApp/UILayer/Onboarding/OnboardingPartViewController.swift
@@ -0,0 +1,95 @@
+//
+//  OnboardingPartViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Азат Ибрагимов on 03.06.2024.
+//
+
+import UIKit
+
+class OnboardingPartViewController: UIViewController {
+    // MARK: - Properties
+    var imageToShow: UIImage? {
+        didSet {
+            imageView.image = imageToShow
+        }
+    }
+    var titleText: String? {
+        didSet {
+            titleLabel.text = titleText
+        }
+    }
+    var descriptionText: String? {
+        didSet {
+            descriptionLabel.text = descriptionText
+        }
+    }
+    
+    // MARK: - Views
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    private lazy var titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.numberOfLines = .zero
+        titleLabel.textAlignment = .center
+        return titleLabel
+    }()
+    private lazy var descriptionLabel: UILabel = {
+        let descriptionLabel = UILabel()
+        descriptionLabel.numberOfLines = .zero
+        descriptionLabel.textAlignment = .center
+        return descriptionLabel
+    }()
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLayout()
+    }
+}
+
+// MARK: - Layout
+private extension OnboardingPartViewController {
+    func setupLayout() {
+        setupView()
+        setupImageView()
+        setupTitle()
+        setupDescription()
+    }
+    func setupView() {
+        view.backgroundColor = AppColors.accentOrange
+    }
+    func setupImageView() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            imageView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    func setupTitle() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 0),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.widthAnchor.constraint(equalToConstant: 200),
+        ])
+        titleLabel.font = .Roboto.bold.size(of: 24)
+        titleLabel.textColor = AppColors.white
+    }
+    func setupDescription() {
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 23),
+            descriptionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            descriptionLabel.widthAnchor.constraint(equalToConstant: 270),
+        ])
+        descriptionLabel.font = .Roboto.regular.size(of: 14)
+        descriptionLabel.textColor = AppColors.white
+    }
+}
